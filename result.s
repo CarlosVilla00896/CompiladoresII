9cbd6cd5c704a16bc2d0e9e00d3ec7faefb74078
carlosvilla00896 <carlosvilla00896@gmail.com>
2021-12-19 05:14:01 -0600

    adds code gen for assignment expression

--- result.s
+++ result.s
@@ -1,240 +1,59 @@
 .data
-string0: .asciiz"a = "
-string1: .asciiz"b = "
-string2: .asciiz"a+b = "
-string3: .asciiz"a-b = "
-string4: .asciiz"a*b = "
-string5: .asciiz"a/b = "
-string6: .asciiz"a>b = "
-string7: .asciiz"a<b = "
-string8: .asciiz"a>=b = "
-string9: .asciiz"a<=b = "
-string10: .asciiz"true&&false = "
+string0: .asciiz"El valor de array[0] es: "
 
 .globl main
 .text
 main: 
-addiu $sp, $sp, -12
+addiu $sp, $sp, -24
 
 sw $ra, 0($sp)
 
-li $t0, 5
-
-li $t1, 100
-
-mult $t0, $t1
-mflo $t0
-
-sw $t0, 4($sp)
 
 li $t0, 2
 
-li $t1, 100
+li $t1, 0
 
-mult $t0, $t1
+li $a0, 4
+mult $a0, $t1
+mflo $t1
+la $t2, 4($sp)
+add $t1, $t1, $t2
+li $t3, 2
+
+sw $t3, 0($t1)
+
+li $t0, 5
+
+li $t1, 1
+
+li $a0, 4
+mult $a0, $t1
+mflo $t1
+la $t2, 4($sp)
+add $t1, $t1, $t2
+li $t4, 5
+
+sw $t4, 0($t1)
+
+
+li $t0, 0
+
+li $a0, 4
+mult $a0, $t0
 mflo $t0
-
-sw $t0, 8($sp)
-
-
-lw $t0, 4($sp)
+la $t1, 4($sp)
+add $t0, $t1, $t0
+lw $t0, 0($t0)
 
 la $a0, string0
 li $v0, 4
 syscall
-addi $a0, $zero, 0xA
-li $v0, 11
 move $a0, $t0
 li $v0, 1
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-
-
-lw $t0, 8($sp)
-
-la $a0, string1
-li $v0, 4
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-move $a0, $t0
-li $v0, 1
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-
-
-lw $t0, 4($sp)
-
-lw $t1, 8($sp)
-
-add $t0, $t0, $t1
-
-la $a0, string2
-li $v0, 4
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-move $a0, $t0
-li $v0, 1
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-
-
-lw $t0, 4($sp)
-
-lw $t1, 8($sp)
-
-sub $t0, $t0, $t1
-
-la $a0, string3
-li $v0, 4
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-move $a0, $t0
-li $v0, 1
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-
-
-lw $t0, 4($sp)
-
-lw $t1, 8($sp)
-
-mult $t0, $t1
-mflo $t0
-
-la $a0, string4
-li $v0, 4
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-move $a0, $t0
-li $v0, 1
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-
-
-lw $t0, 4($sp)
-
-lw $t1, 8($sp)
-
-div $t0, $t1
-mflo $t0
-
-la $a0, string5
-li $v0, 4
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-move $a0, $t0
-li $v0, 1
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-
-
-lw $t0, 4($sp)
-
-lw $t1, 8($sp)
-
-slt $t0, $t1, $t0
-
-la $a0, string6
-li $v0, 4
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-move $a0, $t0
-li $v0, 1
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-
-
-lw $t0, 4($sp)
-
-lw $t1, 8($sp)
-
-slt $t0, $t0, $t1
-
-la $a0, string7
-li $v0, 4
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-move $a0, $t0
-li $v0, 1
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-
-
-lw $t0, 4($sp)
-
-lw $t1, 8($sp)
-
-sge $t0, $t0, $t1
-
-la $a0, string8
-li $v0, 4
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-move $a0, $t0
-li $v0, 1
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-
-
-lw $t0, 4($sp)
-
-lw $t1, 8($sp)
-
-sle $t0, $t0, $t1
-
-la $a0, string9
-li $v0, 4
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-move $a0, $t0
-li $v0, 1
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-
-
-addi $t0, $zero, 1
-
-move $t1, $zero
-
-addi $t2, $zero, 0
-beq $t0, $t2, finalLabel12
-beq $t1, $t2, finalLabel12
-label11:
-addi $t2, $zero, 1
-finalLabel12:
-
-la $a0, string10
-li $v0, 4
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
-move $a0, $t2
-li $v0, 1
-syscall
-addi $a0, $zero, 0xA
-li $v0, 11
+syscall 
 
 
 lw $ra, 0($sp)
-addiu $sp, $sp, 12
+addiu $sp, $sp, 24
 jr $ra
 
